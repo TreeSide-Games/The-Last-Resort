58ce9eb59b841280eec38756b86f6cca080d0bc2
Damian <mrhiwata@wp.pl>
2021-08-25 23:18:09 +0200

    Apteczka System Zdrowia
    Wszystko GIT

---
+++ The Last Resort/Assets/Scripts/Apteczka.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apteczka : MonoBehaviour
+
+{
+    public bool Leczy;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Gracz>())
+        {
+            if (Leczy)
+            {
+                other.GetComponent<Gracz>().DodajZycie();
+                Destroy(gameObject);
+            }
+            else
+            {
+                other.GetComponent<Gracz>().OdejmijZycie();
+                Destroy(gameObject);
+
+            }
+
+        }
+    }
+}
