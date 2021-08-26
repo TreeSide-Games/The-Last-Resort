f2d101bfbb7d9ef6ddc1d1458da2e303d4a2e1a4
Michał Gryczewski <72730947+michalGryczewski1998@users.noreply.github.com>
2021-08-26 22:07:18 +0200

    AI przeciwnika
    Podstawowe AI przeciwnika

--- The Last Resort/Assets/Scripts/Controllers/EnemyController.cs
+++ The Last Resort/Assets/Scripts/Controllers/EnemyController.cs
@@ -7,8 +7,14 @@ public class EnemyController : MonoBehaviour
 {
     public float lookRadius = 10f;
 
+<<<<<<< Updated upstream
     public Transform target;
     public NavMeshAgent agent;
+=======
+    Transform target;
+    NavMeshAgent agent;
+    private Animator animation;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     public void Start()
@@ -20,19 +26,31 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
+<<<<<<< Updated upstream
         float distance = Vector2.Distance(target.position, transform.position);
+=======
+        float distance = Vector3.Distance(target.position, transform.position);
+>>>>>>> Stashed changes
 
         if(distance <= lookRadius)
         {
             agent.SetDestination(target.position);
+<<<<<<< Updated upstream
 
             if(distance <= agent.stoppingDistance)
             {
                 Facetarget();
+=======
+            if (distance <= agent.stoppingDistance)
+            {
+                //Atakuj cel
+                FaceTarget();
+>>>>>>> Stashed changes
             }
         }
     }
 
+<<<<<<< Updated upstream
     public void Facetarget()
     {
         Vector3 direction = (target.position - transform.position).normalized;
@@ -44,5 +62,21 @@ public class EnemyController : MonoBehaviour
     {
         Gizmos.color = Color.blue;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
+=======
+    public void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        animation.SetFloat("Speed", 1f, 0.1f, Time.deltaTime);
+
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+
+>>>>>>> Stashed changes
     }
 }
--- The Last Resort/Assets/Scripts/PlayerManager.cs
+++ The Last Resort/Assets/Scripts/PlayerManager.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
     #region
 
     public static PlayerManager instance;
 
     public void Awake()
+=======
+    #region Singleton
+
+    public static PlayerManager instance;
+
+    private void Awake()
+>>>>>>> Stashed changes
     {
         instance = this;
     }
