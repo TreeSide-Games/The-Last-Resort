c8f88acba73991d9d4cfb691d1c21fa8a627b617
DariusKusnierz <72936095+DariusKusnierz@users.noreply.github.com>
2021-09-06 23:18:31 +0200

    Eksplozje c.d.

--- The Last Resort/Assets/Scripts/Shooting/Explosion.cs
+++ The Last Resort/Assets/Scripts/Shooting/Explosion.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
-    public GameObject explosion;
+    public GameObject explosionParticle;
+    public float explosionRadius = 5f;
+    public float explosionForce = 100f;
 
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag != "Bullet") return;
 
-        Instantiate(explosion, transform.position, transform.rotation);
+        MakeExplosion();
+    }
+
+    public void MakeExplosion()
+    {
+        Instantiate(explosionParticle, transform.position, transform.rotation);
+
+        Collider[] collidersToBreak = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider nearbyObject in collidersToBreak)
+        {
+            EnemyHealth destruction = nearbyObject.GetComponent<EnemyHealth>();
+            if (destruction != null)
+            {
+                destruction.Break();
+            }
+            //Explosion explode = nearbyObject.GetComponent<Explosion>();
+            //if(explode != null)
+            //{
+            //    explode.MakeExplosion();
+            //}
+        }
+
+        Collider[] collidersToForce = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        foreach (Collider nearbyObject in collidersToForce)
+        {
+            Rigidbody rigidbody = nearbyObject.GetComponent<Rigidbody>();
+            if (rigidbody != null)
+            {
+                rigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+            }
+        }
+
         Destroy(gameObject);
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
